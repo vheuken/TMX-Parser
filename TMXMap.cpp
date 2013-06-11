a1c140d16cee7016dd6af3877de5958f15c4c12b
Vincent Heuken <vincent@vincentheuken.com>
2013-06-11 16:04:35 -0700

    added partial implementation of TMXMap

--- TMXMap.cpp
+++ TMXMap.cpp
@@ -1,1 +1,44 @@
 #include "TMXMap.h"
+
+
+TMXMap::TMXMap(std::string pathToMapFile)
+{
+	this->pathToMapFile = pathToMapFile;
+
+	processMap();
+}
+
+void TMXMap::processMap()
+{
+
+}
+
+std::string TMXMap::getVersion() const
+{
+	return version;
+}
+
+std::string TMXMap::getOrientation() const
+{
+	return orientation;
+}
+
+int TMXMap::getWidth() const
+{
+	return width;
+}
+
+int TMXMap::getHeight() const
+{
+	return height;
+}
+
+int TMXMap::getTileWidth() const
+{
+	return tileWidth;
+}
+
+int TMXMap::getTileHeight() const
+{
+	return tileHeight;
+}
--- TMXMap.h
+++ TMXMap.h
@@ -1,10 +1,43 @@
 #ifndef TMXMAP_H
 #define TMXMAP_H
 
+#include <string>
+
+#include "rapidxml-1.13/rapidxml.hpp"
+
 class TMXMap 
 {
 public:
-    TMXMap();
+    TMXMap(std::string pathToMapFile);
+
+	std::string getVersion() const;
+	std::string getOrientation() const;
+
+	int getWidth() const;
+	int getHeight() const;
+
+	int getTileWidth() const;
+	int getTileHeight() const;
+
+
+private:
+	std::string pathToMapFile;
+
+	std::string version;
+
+	// Supports "orthogonal", "isometric", 
+	// and "staggered" orientations
+	std::string orientation; 
+
+	// width and height are measured in tiles
+	int width, height;
+
+	int tileWidth, tileHeight;
+
+	// Can contain: properties, tileset, layer, objectgroup, imagelayer
+
+	void processMap();
+
 };
 
 #endif // TMXMAP_H
