c5783c29b89951405f06d7f4c0a75eca59148b25
Vincent Heuken <vincent@vincentheuken.com>
2013-06-11 18:16:42 -0700

    moved BOOST_TEST_MODULE definition

--- tests/TMXMap_test.cpp
+++ tests/TMXMap_test.cpp
@@ -1,8 +1,8 @@
 #define BOOST_TEST_MAIN
-#define BOOST_TEST_MODULE TMXMap
 #if !defined( WIN32 )
     #define BOOST_TEST_DYN_LINK
 #endif
+#define BOOST_TEST_MODULE TMXMap
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE( my_test ) {
